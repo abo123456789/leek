1f8461be36c5d8db591114f2095de1536f1e2bbc
abo123456789 <abcdef123456chen@sohu.com>
2019-09-27 23:46:20 +0800

    Update RedisQueue.py

--- RedisQueue.py
+++ RedisQueue.py
@@ -202,7 +202,7 @@ class RedisPublish(object):
 class BoundedThreadPoolExecutor(ThreadPoolExecutor):
     def __init__(self, max_workers=None, thread_name_prefix=''):
         super().__init__(max_workers, thread_name_prefix)
-        self._work_queue = queue.Queue(max_workers * 2)
+        self._work_queue = queue.Queue(max_workers * 1)
 
     def submit(self, fn, *args, **kwargs):
         with self._shutdown_lock:
