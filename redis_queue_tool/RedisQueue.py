c89a762da0f1c4244f737134e98b6a88115c107b
codecc <abcdef123456chen@sohu.com>
2020-08-03 17:30:08 +0800

    设置RedisQueue过期提醒

--- redis_queue_tool/RedisQueue.py
+++ redis_queue_tool/RedisQueue.py
@@ -2,4 +2,7 @@
 # @Time    : 2020/8/3 16:51
 # @Author  : CC
 # @Desc    : RedisQueue.py 用来兼容旧版本,请勿使用这个文件导入相应的包
+import warnings
+
+warnings.warn("RedisQueue is deprecated", DeprecationWarning)
 from redis_queue_tool import task_deco, RedisPublish, RedisCustomer
