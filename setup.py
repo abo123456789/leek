0f8a8ed7d2b3286eb2bc7af2b10d3d01a2b2fbef
zhangxy2020 <zhangxinyuan@yimian.com.cn>
2020-10-26 17:14:21 +0800

    fix:fix case stop the craw  then break the while

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='redis-queue-tool',
-    version='4.6.5',
+    version='4.6.6',
     # name='leek',
     # version='1.0.3',
     description=(
