516e7078e969ce8cb57f719e3eed119baa470b4b
codecc <abcdef123456chen@sohu.com>
2021-04-25 22:41:08 +0800

    add support task_expires params

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='leek',
-    version='1.1.6',
+    version='1.1.7',
     description=(
         'Task publishing and consumption Middleware'
     ),
